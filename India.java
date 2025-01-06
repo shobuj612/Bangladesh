eb148476d7bc429c81691263392cc859ec250d1e
shobuj <iqramulislam55@gmail.com>
2025-01-06 12:54:13 +0600

    Add India.java

---
+++ India.java
@@ -0,0 +1,12 @@
+public 
+class India {
+    public static void main(String[] args) {
+        
+        sum(10,25);
+    }
+    
+    public static int sum(int a,int b){
+
+        return a*b;
+    }
+}
