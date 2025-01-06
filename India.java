efae06b182b579b092ed225fef9a8e70331cfc0b
shobuj <iqramulislam55@gmail.com>
2025-01-06 13:09:36 +0600

    Add India.java

--- India.java
+++ India.java
@@ -3,7 +3,11 @@ class India {
     public static void main(String[] args) {
         
         sum(10,25);
+
+      System.out.println("i love my courntry very mush");
     }
+
+
     
     public static int sum(int a,int b){
 
